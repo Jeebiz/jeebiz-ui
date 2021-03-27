6f0c01c1530967a7155d8a7dd1d5b76824ece37d
wandl <hnxxyhcwdl1003@163.com>
2021-03-27 23:21:51 +0800

    PC端对接

--- jeebiz-ui-layui/src/main/webapp/src/config.js
+++ jeebiz-ui-layui/src/main/webapp/src/config.js
@@ -14,7 +14,7 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function (exports) {
 		engine: '.html', //  视图文件后缀名
 		pageTabs: false, //  是否开启页面选项卡功能。单页版不推荐开启
 
-		name: 'LayUI Admin Pro',
+		name: 'Jeebiz Admin Pro',
 		tableName: 'jeebizAdmin', //  本地存储表名
 		MOD_NAME: 'admin', //  模块事件名
 
--- jeebiz-ui-layui/src/main/webapp/src/controller/common.js
+++ jeebiz-ui-layui/src/main/webapp/src/controller/common.js
@@ -22,14 +22,17 @@ layui.define(function(exports){
   
   //退出
   admin.events.logout = function(){
+    // 清空本地记录的 token，并跳转到登入页
+    admin.exit();
+    return;
     //执行退出接口
     admin.req({
-      url: './json/user/logout.js'
+      url: layui.setter.prefix + '/logout'
       ,type: 'get'
       ,data: {}
       ,done: function(res){ //这里要说明一下：done 是只有 response 的 code 正常才会执行。而 succese 则是只要 http 为 200 就会执行
         
-        //清空本地记录的 token，并跳转到登入页
+        // 清空本地记录的 token，并跳转到登入页
         admin.exit();
       }
     });
